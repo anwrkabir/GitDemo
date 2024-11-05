c65c981aae7fc43a79a22de49c4dec2fb88cd962
md anwarul kabir <anwrkabir@gmail.com>
2024-11-04 23:09:54 -0500

    third commit from dev x

---
+++ src/main/java/RSFrameWork/pageObjects/Test2.java
@@ -0,0 +1,7 @@
+package RSFrameWork.pageObjects;
+
+public class Test2 {
+    public void testClone2(){
+        System.out.println("This method is created by dev x to test the git clone_push and pull" );
+    }
+}
