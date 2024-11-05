23c553eaccf5f4d1ae6b263e0acb811f1fcdb304
md anwarul kabir <anwrkabir@gmail.com>
2024-11-04 21:01:54 -0500

    this is second commit from GitDemon by developer y

---
+++ src/main/java/RSFrameWork/pageObjects/Test1.java
@@ -0,0 +1,7 @@
+package RSFrameWork.pageObjects;
+
+public class Test1 {
+    public void testClone(){
+        System.out.println("This method is created to test the git clone_push and pull" );
+    }
+}
