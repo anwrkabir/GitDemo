b1f14ce2a08fbd07feadd5b7c4db77179e4fff19
md anwarul kabir <anwrkabir@gmail.com>
2024-11-05 23:22:10 -0500

    fifth commit by dev x in develop at gitx folder

---
+++ src/main/java/RSFrameWork/pageObjects/Test4.java
@@ -0,0 +1,8 @@
+package RSFrameWork.pageObjects;
+
+public class Test4 {
+
+    public void testClone3(){
+        System.out.println("This method is created in the develop branch by dev x in gitx " );
+    }
+}
