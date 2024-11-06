d57249e1e9c2980349838748057c20d23961f7a8
md anwarul kabir <anwrkabir@gmail.com>
2024-11-06 15:36:43 -0500

    commit in develop2 branch by dev y

---
+++ src/main/java/RSFrameWork/pageObjects/Test6.java
@@ -0,0 +1,10 @@
+package RSFrameWork.pageObjects;
+
+public class Test6 {
+
+    public  void testMergeConflict(){
+        System.out.println("this file is created in GitDemo to test the mergeConflit by dev ");
+
+        System.out.println("This code is created in develop2");
+    }
+}
