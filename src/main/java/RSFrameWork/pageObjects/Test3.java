819164ea695c84790ad3452c75f3bac864c2644e
md anwarul kabir <anwrkabir@gmail.com>
2024-11-05 20:38:55 -0500

    fourth commit by dev y in develop

---
+++ src/main/java/RSFrameWork/pageObjects/Test3.java
@@ -0,0 +1,8 @@
+package RSFrameWork.pageObjects;
+
+public class Test3 {
+
+    public void testClone3(){
+        System.out.println("This method is created in the develop branch by dev y" );
+    }
+}
